bc20cf0efaa52acfdc1d4a635fc1fd0274e209a6
Dagan Martinez <dmartinez@starry.com>
2022-01-08 09:45:12 -0500

    Use private methods/fields

--- board_state.js
+++ board_state.js
@@ -43,7 +43,7 @@ export class Point {
     }
 
     // Return all orthogonally adjacent points 
-    getOrthogonalsByDimension(dimension) {
+    #getOrthogonalsByDimension(dimension) {
         const orthogonals = [];
         const coordinates = [];
         for (let i = 0; i < dimension; i++) {
@@ -60,11 +60,11 @@ export class Point {
     }
 
     get3dOrthogonals() {
-        return this.getOrthogonalsByDimension(3);
+        return this.#getOrthogonalsByDimension(3);
     }
 
     get2dOrthogonals() {
-        return this.getOrthogonalsByDimension(2);
+        return this.#getOrthogonalsByDimension(2);
     }
 
     // Check if all coordinates are inbetween min and max
@@ -90,52 +90,59 @@ export class Point {
 
 
 export class BoardState {
+    #state;
+    #mode_3d;
+    #max;
+    #min;
+    #next_group_id;
+    #group_liberty_map;
+    #point_group_map;
+
     constructor(grid_size = DEFAULT_GRID_SIZE, mode_3d = false) {
-        this.mode_3d = mode_3d;
-        this.grid_size = grid_size;
-        this.max = (grid_size - 1) / 2;
-        this.min = -this.max
+        this.#mode_3d = mode_3d;
+        this.#max = (grid_size - 1) / 2;
+        this.#min = -this.#max
 
         this.reset();
     }
 
     reset() {
-        this._state = {};
-        this.resetLibertyCache();
+        this.#state = {};
+        this.#resetLibertyCache();
     }
 
-    resetLibertyCache() {
-        this.next_group_id = 1;
-        this.group_liberty_map = {};
-        this.point_group_map = {};
+    #resetLibertyCache() {
+        this.#next_group_id = 1;
+        this.#group_liberty_map = {};
+        this.#point_group_map = {};
 
-        this.countGroups();
+        this.#countGroups();
     }
 
-    set(point, color) {
-        this.guard(point);
-        this._state[point.getValue()] = color;
+    #set(point, color) {
+        this.#guard(point);
+        this.#state[point.getValue()] = color;
 
-        for (let adjacent of this.getOrthogonals(point)) {
+        for (let adjacent of this.#getOrthogonals(point)) {
             if (this.get(adjacent)) {
-                this.resetLibertyCache();
+                this.#resetLibertyCache();
                 return;
             }
         }
-        this.countGroups();
+        this.#countGroups();
     }
 
     remove(point) {
-        this.set(point, null);
+        this.#set(point, null);
     }
 
     get(point) {
-        this.guard(point);
-        return this._state[point.getValue()];
+        this.#guard(point);
+        return this.#state[point.getValue()];
     }
 
     getGroupLiberties() {
-        return this.group_liberty_map;
+        return this.#group_liberty_map;
     }
 
     getAllPoints() {
@@ -144,10 +151,10 @@ export class BoardState {
         }
 
         const points = [];
-        for (let x = this.min; x <= this.max; x++) {
-            for (let y = this.min; y <= this.max; y++) {
-                if (this.mode_3d) {
-                    for (let z = this.min; z <= this.max; z++) {
+        for (let x = this.#min; x <= this.#max; x++) {
+            for (let y = this.#min; y <= this.#max; y++) {
+                if (this.#mode_3d) {
+                    for (let z = this.#min; z <= this.#max; z++) {
                         points.push(new Point(x, y, z));
                     }
                 } else {
@@ -173,36 +180,36 @@ export class BoardState {
         return false;
     }
 
-    guard(point) {
-        if (!point.inBounds(this.min, this.max)) {
+    #guard(point) {
+        if (!point.inBounds(this.#min, this.#max)) {
             throw new Error("Out of bounds");
         }
     }
 
-    getOrthogonals(point) {
+    #getOrthogonals(point) {
         let orthogonals;
-        if (this.mode_3d) {
+        if (this.#mode_3d) {
             orthogonals = point.get3dOrthogonals();
         } else {
             orthogonals = point.get2dOrthogonals();
         }
-        return orthogonals.filter((x) => x.inBounds(this.min, this.max))
+        return orthogonals.filter((x) => x.inBounds(this.#min, this.#max))
     }
 
-    countGroups() {
+    #countGroups() {
         for (let point of this.getAllPoints()) {
             const point_value = point.getValue();
-            if (!this._state[point_value] || this.point_group_map[point_value]) {
+            if (!this.#state[point_value] || this.#point_group_map[point_value]) {
                 continue;
             }
 
-            const group_id = this.next_group_id++;
-            this.countLibertiesForGroup(point, group_id);
+            const group_id = this.#next_group_id++;
+            this.#countLibertiesForGroup(point, group_id);
 
         }
     }
 
-    countLibertiesForGroup(point, group_id) {
+    #countLibertiesForGroup(point, group_id) {
         const color = this.get(point);
         const searched = new Set();
         const queue = [point]
@@ -211,7 +218,7 @@ export class BoardState {
         while (queue.length) {
             const subject = queue.pop();
             const subject_value = subject.getValue();
-            const subject_color = this._state[subject_value];
+            const subject_color = this.#state[subject_value];
 
             if (searched.has(subject_value)) {
                 continue;
@@ -224,12 +231,12 @@ export class BoardState {
             }
 
             if (subject_color === color) {
-                this.point_group_map[subject_value] = group_id;
-                queue.push(...this.getOrthogonals(subject))
+                this.#point_group_map[subject_value] = group_id;
+                queue.push(...this.#getOrthogonals(subject))
             }
         }
 
-        this.group_liberty_map[group_id] = {
+        this.#group_liberty_map[group_id] = {
             liberties,
             color
         };
@@ -240,8 +247,8 @@ export class BoardState {
 
     countLiberties(point) {
         const point_value = point.getValue();
-        const group_id = this.point_group_map[point_value];
-        const liberties = this.group_liberty_map[group_id].liberties;
+        const group_id = this.#point_group_map[point_value];
+        const liberties = this.#group_liberty_map[group_id].liberties;
         if (group_id == null || liberties == null) {
             throw new Error("Cannot count liberties at this point: ", point);
         }
@@ -249,13 +256,13 @@ export class BoardState {
     }
 
     moveIsLegal(point, color) {
-        this.guard(point);
+        this.#guard(point);
 
         if (this.get(point)) {
             return false;
         }
 
-        const adjacents = this.getOrthogonals(point);
+        const adjacents = this.#getOrthogonals(point);
         for (let adjacent of adjacents) {
             const adjacent_color = this.get(adjacent);
             // To be legal, there needs to be at least one free adjacent,
@@ -277,11 +284,11 @@ export class BoardState {
     }
 
     place(point, color) {
-        this.guard(point);
+        this.#guard(point);
         if (!this.moveIsLegal(point, color)) {
             return false;
         }
-        this.set(point, color);
+        this.#set(point, color);
         return true;
     }
 }
--- human.js
+++ human.js
@@ -9,7 +9,6 @@ import {
     BOARD_WIDTH,
     BOARD_HEIGHT,
 } from "./constants.js";
-// Translate canvas points to grid points
 
 function inDelta(value) {
     const diff = Math.abs(value % 1);
@@ -20,36 +19,38 @@ function absoluteToCanvas(canvas, absolute_x, absolute_y) {
     const bbox = canvas.getBoundingClientRect();
     const scaleX = canvas.width / bbox.width;
     const scaleY = canvas.height / bbox.height;
-    let [x, y] = [(absolute_x - bbox.left) * scaleX, (absolute_y - bbox.top) * scaleY];
-    return [x, y];
+    return [(absolute_x - bbox.left) * scaleX, (absolute_y - bbox.top) * scaleY];
 }
 
 export default class Human {
+    #game;
+
     constructor(game) {
-        this.game = game;
+        this.#game = game;
     }
 
     useTimer() {
         return false;
     }
 
-    revTranslate(x, y) {
+    // Translate canvas points to grid points
+    #revTranslate(x, y) {
         x -= BUFFER;
         y -= BUFFER;
-        x = (x * (this.game.grid_size - 1)) / BOARD_WIDTH;
-        y = (y * (this.game.grid_size - 1)) / BOARD_HEIGHT;
-        x -= this.game.highest_index;
-        y -= this.game.highest_index;
+        x = (x * (this.#game.grid_size - 1)) / BOARD_WIDTH;
+        y = (y * (this.#game.grid_size - 1)) / BOARD_HEIGHT;
+        x -= this.#game.highest_index;
+        y -= this.#game.highest_index;
         return [x, y]
     }
 
-    setCanvasesClickable(yes, state, color, callback) {
-        const canvases = this.game.canvases;
-        for (let i = 0; i < this.game.num_planes; i++) {
+    #setCanvasesClickable(yes, state, color, callback) {
+        const canvases = this.#game.canvases;
+        for (let i = 0; i < this.#game.num_planes; i++) {
             if (yes) {
                 canvases[i].onclick = (e) => {
                     const [_x, _y] = absoluteToCanvas(e.target, e.clientX, e.clientY)
-                    let [x, y] = this.revTranslate(_x, _y)
+                    let [x, y] = this.#revTranslate(_x, _y)
 
                     if (inDelta(x) && inDelta(y)) {
                         x = Math.round(x);
@@ -61,7 +62,7 @@ export default class Human {
                             x = y
                             y = -t;
                         }
-                        const offset = (this.game.num_planes - 1) / 2
+                        const offset = (this.#game.num_planes - 1) / 2
                         const point = new Point(x, y, i - offset);
 
                         if (state.moveIsLegal(point, color)) {
@@ -76,8 +77,8 @@ export default class Human {
     }
 
     nextMove(state, color, callback) {
-        this.setCanvasesClickable(true, state, color, (move) => {
-            this.setCanvasesClickable(false, state);
+        this.#setCanvasesClickable(true, state, color, (move) => {
+            this.#setCanvasesClickable(false, state);
             callback(move);
         });
     }
